846aaf3e66a0c18ffe3578dc8340b0f08cee48af
Kieran-Shaw <71037357+Kieran-Shaw@users.noreply.github.com>
2023-09-07 17:42:17 -0400

    cold-start: setting up, initial code

---
+++ app.py
@@ -0,0 +1,77 @@
+import logging
+import os
+
+from dotenv import load_dotenv
+from flask import Flask, jsonify, request
+
+from config.config import DevelopmentConfig, ProductionConfig
+from methods.cold_start import ColdStart
+
+
+# configure logging
+def configure_logging(app):
+    log_level = app.config.get("LOG_LEVEL", logging.INFO)
+
+    if os.getenv("ENVIRONMENT") == "aws":
+        # AWS environment, let CloudWatch handle logging.
+        logging.basicConfig(
+            level=log_level,
+            format="%(asctime)s - %(levelname)s - %(message)s",
+            handlers=[logging.StreamHandler()],
+        )
+    else:
+        # local or other environments, file and stream based logging.
+        logging.basicConfig(
+            level=log_level,
+            format="%(asctime)s - %(levelname)s - %(message)s",
+            handlers=[
+                logging.StreamHandler(),
+                logging.FileHandler("logs/application.log"),
+            ],
+        )
+
+
+# create application
+def create_app():
+    app = Flask(__name__)
+    app.logger = logging.getLogger(__name__)
+
+    load_dotenv()  # load environment variables from .env file
+
+    env = os.getenv("FLASK_ENV", "production")
+
+    if env == "development":
+        app.config.from_object(DevelopmentConfig)
+    else:
+        app.config.from_object(ProductionConfig)
+
+    configure_logging(app)
+
+    @app.route("/")
+    def base_request():
+        app.logger.info("Base URL")
+        return jsonify(f"Base Endpoint, No Functionality"), 404
+
+    @app.route("/cold-start")
+    def cold_start():
+        try:
+            # parse input
+            data = request.json
+            # instantiate cold-start
+            cold_start = ColdStart(data=data)
+            # predict
+            predicted_plans = cold_start.predict_plans()
+
+            return predicted_plans
+
+        except Exception as e:
+            app.logger.error("Exception occurred", exc_info=True)
+            return jsonify({"error": "An error occurred"}), 500
+
+    return app
+
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ methods/cold_start.py
@@ -0,0 +1,109 @@
+import logging
+
+import Levenshtein
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+class ColdStart:
+    def __init__(self, data: dict, logger: logging.Logger) -> None:
+        self.census_plan_admin_name = data["census_plan_admin_name"]
+        self.census_carrier_name = data["census_carrier_name"]
+        self.available_plans = data["available_plans"]
+        self.vectorizer = TfidfVectorizer()
+
+        # initialize the logger
+        self.logger = logger
+        self.logger.info(
+            "Initialized cold start",
+            extra={
+                "client_name": self.census_carrier_name,
+                "client_id": self.census_plan_admin_name,
+            },
+        )
+
+    def predict_plans(self):
+        available_plans_df = self._available_plans_to_df()
+        most_similar_carrier_name = self._best_match_carrier(df=available_plans_df)
+        filtered_df = self._filter_dataframe_for_carrier(
+            df=available_plans_df, similar_carrier_name=most_similar_carrier_name
+        )
+        top_three_similar = self._return_top_three_similar(filtered_df=filtered_df)
+
+        return top_three_similar
+
+    def _available_plans_to_df(self):
+        df = pd.DataFrame(self.available_plans)
+        self.logger.info(
+            "Compelted available plans to dataframe", extra={"length": len(df)}
+        )
+        return df
+
+    def _best_match_carrier(self, df: pd.DataFrame):
+        # list of unique carriers in df
+        unique_carrier_names = list(df["carrier"].unique())
+        # Calculate Levenshtein distance to known carrier names
+        distances = [
+            Levenshtein.distance(self.census_carrier_name, carrier)
+            for carrier in unique_carrier_names
+        ]
+
+        # Find the most similar carrier name based on minimum distance
+        most_similar_index = np.argmin(distances)
+        most_similar_carrier_name = unique_carrier_names[most_similar_index]
+
+        # log out the most similar
+        self.logger.info(
+            "Most similar carrier name complete",
+            extra={
+                "census_carrier)name": self.census_carrier_name,
+                "most_similar_carrier_name": most_similar_carrier_name,
+            },
+        )
+
+        return most_similar_carrier_name
+
+    def _filter_dataframe_for_carrier(
+        self, df: pd.DataFrame, similar_carrier_name: str
+    ):
+        # copy dataframe
+        new_df = df.copy()
+
+        # filter new dataframe
+        filtered_df = new_df[new_df["carrier"] == similar_carrier_name].reset_index(
+            drop=True
+        )
+
+        # log out the filtered dataframe
+        self.logger.info(
+            "Dataframe filtered for most similar carrier",
+            extra={"length": len(filtered_df)},
+        )
+
+        return filtered_df
+
+    def _return_top_three_similar(self, filtered_df: pd.DataFrame):
+        # Vectorize the filtered_df names
+        name_vectors = self.vectorizer.fit_transform(filtered_df["name"])
+
+        # Find the most similar plan
+        query_vector = self.vectorizer.transform([self.census_carrier_name])
+        similarities = cosine_similarity(query_vector, name_vectors)
+
+        # Get the indices of the top three most similar plans
+        top_three_indices = similarities.argsort()[0][-3:][::-1]
+
+        # Get the most similar plan names and IDs
+        top_three_names = filtered_df.loc[top_three_indices, "name"].values
+        top_three_ids = filtered_df.loc[top_three_indices, "id"].values
+
+        # Package the information into a dictionary
+        response_object = {
+            "1": {"name": top_three_names[0], "id": top_three_ids[0]},
+            "2": {"name": top_three_names[1], "id": top_three_ids[1]},
+            "3": {"name": top_three_names[2], "id": top_three_ids[2]},
+        }
+
+        return response_object
